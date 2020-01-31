203f0f8bf1eea46b77f89e165f866128500a1467
Lyza <matocinoslyza@gmail.com>
2020-01-31 08:08:18 +0800

    First Commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  onChangeText(text) {
+    this.setState({
+      text: text
+    });
+    
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <TextInput
+          onChangeText={ (text) => this.onChageText(text)}
+          placeholder={ 'Enter text here' }
+          value={this.state.text}
+        />
+      <TouchableOpacity
+        style={{marginTop: 20}}
+        onPress={ () => alert(this.state.text)}
+        ><Text>Press Me</Text>
+      </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
